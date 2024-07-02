504e8886678d19da0056786d22c8fc9a357eda47
xXLucasmorasXx <lucasmoras63@gmail.com>
2024-07-02 15:29:22 -0300

    teste#1

---
+++ src/trabalhoarduino/TrabalhoArduino.java
@@ -0,0 +1,13 @@
+package trabalhoarduino;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class TrabalhoArduino {
+
+
+    public static void main(String[] args) {
+        String arvore;
+    }
+    
+}
